ed5dc74ee5ec95401c087833e9ab8cff0c329b8b
Magnus Burenius <magnus.burenius@gmail.com>
2019-03-31 16:18:06 +0200

    Update header description

--- header.js
+++ header.js
@@ -1,4 +1,4 @@
-const TEXT = "Press CTRL+R to generate a new random image. View page source to see the Javascript that generates the image.";
+const TEXT = "Refresh page to generate a new random image. View page source to see the Javascript that generates the image.";
 const div = document.createElement("div");
 div.style = "padding-bottom:100px"
 const text = document.createTextNode(TEXT);
